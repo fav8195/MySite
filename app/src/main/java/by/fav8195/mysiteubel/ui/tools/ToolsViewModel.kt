e6292d940c4a18bc56daa7314782f27045192abc
Work <filinkov@u-plast.by>
2020-01-13 13:25:22 +0300

    Initial commit

---
+++ app/src/main/java/by/fav8195/mysiteubel/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package by.fav8195.mysiteubel.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
