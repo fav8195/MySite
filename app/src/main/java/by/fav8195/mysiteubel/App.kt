5cb09907a68470604db37d206bd713662f60c6de
Work <filinkov@u-plast.by>
2020-01-21 10:41:11 +0300

    Урок 5 (оповещения не проверено)

---
+++ app/src/main/java/by/fav8195/mysiteubel/App.kt
@@ -0,0 +1,17 @@
+package by.fav8195.mysiteubel
+
+import android.app.Application;
+import com.onesignal.OneSignal
+
+public class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+            .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+            .unsubscribeWhenNotificationsAreDisabled(true)
+            .init()
+
+    }
+
+}
