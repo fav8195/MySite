665beda9766bbdcd5f6806435ffed909582f3277
Work <filinkov@u-plast.by>
2020-01-28 11:47:48 +0300

    Удалены уведомления. Собирается апк!

--- app/src/main/java/by/fav8195/mysiteubel/App.kt
+++ app/src/main/java/by/fav8195/mysiteubel/App.kt
@@ -1,16 +1,18 @@
 package by.fav8195.mysiteubel
 
 import android.app.Application;
-import com.onesignal.OneSignal
+//import com.onesignal.OneSignal
 
 public class App : Application() {
     override fun onCreate() {
         super.onCreate()
         // OneSignal Initialization
+/*
         OneSignal.startInit(this)
             .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
             .unsubscribeWhenNotificationsAreDisabled(true)
             .init()
+*/
 
     }
 
